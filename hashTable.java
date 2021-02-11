d7c3c01843ed81ba7ea2705e21b5d074d842452e
Anya <annagapyuk@gmail.com>
2021-02-10 19:08:21 -0500

    updated displayTheStack function

--- hashTable.java
+++ hashTable.java
@@ -65,6 +65,47 @@ public class HashFunction {
         int increment = 0;
         for(int m = 0; m < 3; m++){
             increment += 10;
+
+             for (int n = 0; n < 71; n++)
+
+                System.out.print("-");
+               System.out.println();
+
+            for (int n = increment - 10; n < increment; n++) {
+
+                System.out.format("| %3s " + " ", n);
+            }
+
+            System.out.println("|");
+
+            for (int n = 0; n < 71; n++)
+
+                System.out.print("-");
+
+            System.out.println();
+
+            for (int n = increment - 10; n < increment; n++) {
+
+
+                if (theArray[n].equals("-1"))
+
+                    System.out.print("|      ");
+                else
+
+                    System.out.print(String.format("| %3s " + " ", theArray[n]));
+            }
+
+
+            System.out.println("|");
+
+            for (int n = 0; n < 71; n++)
+
+                System.out.print("-");
+
+ 
+
+            System.out.println();
+
         }
     }
 }
